6dbc93ec78a046bdcf31547cd2b232245e98fa8c
michalHorovitz <michal.horovits@gmail.com>
2022-03-03 11:26:27 +0200

    update bonus

--- OOP-HW2-Triangle/src/TestHW2OtherMethods.java
+++ OOP-HW2-Triangle/src/TestHW2OtherMethods.java
@@ -1,5 +1,3 @@
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
@@ -35,7 +33,7 @@ public class TestHW2OtherMethods {
 		assertTrue(HW2Utils.areEqual(tr2.getPerimeter(), 3));
 		assertTrue(HW2Utils.areEqual(tr3.getPerimeter(), 6 / (Math.sqrt(3))));
 		assertTrue(HW2Utils.areEqual(tr4.getPerimeter(), 6 / (Math.sqrt(3))));
-	} 
+	}
 
 	@Test
 	public void testIsEqual() {
@@ -61,80 +59,5 @@ public class TestHW2OtherMethods {
 		assert !tr4.isEqual(tr1);
 		assert !tr4.isEqual(tr2);
 	}
-
-	@Test
-	public void testContainsPoint() {
-		assertTrue(tr1.contains(new Point(-0.4, -0.28)));
-		assertTrue(tr1.contains(new Point(0, 0.57)));
-		assertTrue(tr1.contains(new Point(0, 0)));
-		assertTrue(tr1.contains(new Point(-0.1, -0.1)));
-		assertFalse(tr1.contains(new Point(0, 2)));
-		assertFalse(tr1.contains(new Point(0.3, 0.5)));
-		
-		assertTrue(tr2.contains(new Point(-0.4, -0.28)));
-		assertTrue(tr2.contains(new Point(0, 0.57)));
-		assertTrue(tr2.contains(new Point(0, 0)));
-		assertTrue(tr2.contains(new Point(-0.1, -0.1)));
-		assertFalse(tr2.contains(new Point(0, 2)));
-		assertFalse(tr2.contains(new Point(0.3, 0.5)));
-		
-			
-		assertTrue(tr3.contains(new Point(-1 / (Math.sqrt(3)), 0)));
-		assertTrue(tr3.contains(new Point(0, -1)));
-		assertTrue(tr3.contains(new Point(0, 0)));
-		assertTrue(tr3.contains(new Point(-0.1, -0.1)));
-		assertFalse(tr3.contains(new Point(0, 2)));
-		assertFalse(tr3.contains(new Point(0.3, 0.5)));
-		
-		assertTrue(tr4.contains(new Point(-1 / (Math.sqrt(3)), 0)));
-		assertTrue(tr4.contains(new Point(0, -1)));
-		assertTrue(tr4.contains(new Point(0, 0)));
-		assertTrue(tr4.contains(new Point(-0.1, -0.1)));
-		assertFalse(tr4.contains(new Point(0, 2)));
-		assertFalse(tr4.contains(new Point(0.3, 0.5)));
-	}
-	
-
-	@Test
-	public void testContainsTriangle() {
-		assertTrue(tr1.contains(tr1));
-		assertTrue(tr2.contains(tr2));
-		assertTrue(tr3.contains(tr3));
-		assertTrue(tr4.contains(tr4));
-		assertTrue(tr1.contains(tr2));
-		assertTrue(tr2.contains(tr1));
-		assertTrue(tr3.contains(tr4));
-		assertTrue(tr4.contains(tr3));
-		
-		tr1.scale(0.5);
-		assertTrue(tr2.contains(tr1));
-		tr2.scale(0.3);
-		assertTrue(tr1.contains(tr2));
-		
-		assertFalse(tr1.contains(tr3));
-		assertFalse(tr1.contains(tr4));
-		assertFalse(tr2.contains(tr3));
-		assertFalse(tr2.contains(tr4));
-		assertFalse(tr3.contains(tr1));
-		assertFalse(tr3.contains(tr2));
-		assertFalse(tr4.contains(tr1));
-		assertFalse(tr4.contains(tr2));
-		
-		tr1.scale(3);
-		tr2.scale(4);
-		assertFalse(tr1.contains(tr3));
-		assertFalse(tr1.contains(tr4));
-		assertFalse(tr2.contains(tr3));
-		assertFalse(tr2.contains(tr4));
-		
-		tr1.scale(3);
-		tr2.scale(3);
-		assertTrue(tr1.contains(tr3));
-		assertTrue(tr1.contains(tr4));
-		assertTrue(tr2.contains(tr3));
-		assertTrue(tr2.contains(tr4));
-	}
-
-
 
 }
